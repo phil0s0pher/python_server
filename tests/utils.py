93f9b1237d73648f09a7fa3edee43cafda7a5486
phil0s0pher <c.lambert1213@gmail.com>
2017-12-01 11:41:08 -0800

    refactor: Refactored inputs to functions to use generator functions

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from sqlalchemy import Table, Column, String, MetaData, DateTime, Boolean
 from datetime import datetime
+from names import get_first_name, get_last_name
 
 METADATA = MetaData()
 ASKANYTHING_TABLE = Table('askanythings', METADATA,
@@ -16,65 +17,45 @@ ASKANYTHING_VOTE_TABLE = Table('askanythingvotes', METADATA,
                                    'question_id', String(50), nullable=False),
                                Column('voter', String(75)))
 
-ASKANYTHINGS_DATA = [{
-    "id": 1,
-    "updated_at": datetime.now(),
-    "question": "Something",
-    "reviewed": True,
-    "authorized": True
-}, {
-    "id": 2,
-    "updated_at": datetime.now(),
-    "question": "Something Else",
-    "reviewed": True,
-    "authorized": True
-}, {
-    "id": 3,
-    "updated_at": datetime.now(),
-    "question": "Something More",
-    "reviewed": True,
-    "authorized": True
-}]
 
-ASKANYTHINGVOTES_DATA = [{
-    "id": 1,
-    "updated_at": datetime.now(),
-    "question_id": 1,
-    "voter": "ryan.rabello"
-}, {
-    "id": 2,
-    "updated_at": datetime.now(),
-    "question_id": 2,
-    "voter": "ryan.rabello"
-}, {
-    "id": 3,
-    "updated_at": datetime.now(),
-    "question_id": 3,
-    "voter": "ryan.rabello"
-}]
+def gen_askanythings(number=5):
+    for i in xrange(number):
+        yield {
+            "id": i,
+            "updated_at": datetime.now(),
+            "question": "Something_{}".format(i),
+            "reviewed": True,
+            "authorized": True
+        }
+
+
+def gen_askanythingvotes(number=5):
+    for i in xrange(number):
+        yield {
+            "id": i,
+            "updated_at": datetime.now(),
+            "question_id": 1,
+            "voter": get_first_name() + '.' + get_last_name()
+        }
+
+
+def edit(generator, changes):
+    for i, record in enumerate(generator):
+        if i in changes.iterkeys():
+            record.update(changes[i])
+
+        yield record
 
 
 @contextmanager
-def askanything(conn, askanythings=ASKANYTHINGS_DATA):
+def askanything(conn, askanythings=list(gen_askanythings())):
     conn.execute(ASKANYTHING_TABLE.insert(), askanythings)
     yield askanythings
     conn.execute(ASKANYTHING_TABLE.delete())
 
 
 @contextmanager
-def askanthingvote(conn, askanythingvotes=ASKANYTHINGVOTES_DATA):
+def askanthingvote(conn, askanythingvotes=list(gen_askanythingvotes())):
     conn.execute(ASKANYTHING_VOTE_TABLE.insert(), askanythingvotes)
     yield askanythingvotes
     conn.execute(ASKANYTHING_VOTE_TABLE.delete())
-
-
-def gen_askanythings(number=5):
-    arr = []
-    for i in number:
-        arr.push({
-            "id": i,
-            "updated_at": datetime.now(),
-            "question": "Something_{}".format(i),
-            "reviewed": True,
-            "authorized": True
-        })
